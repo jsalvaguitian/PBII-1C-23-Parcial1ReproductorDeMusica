5806875e6b2e6d43d6455c621280657e5201195b
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2023-05-14 20:12:03 -0300

    se agrega test para no registrar user con nombre de usuario duplicado

--- test/ar/edu/unlam/pb2/parcial1/reproductor/tdd/ReproductorTest.java
+++ test/ar/edu/unlam/pb2/parcial1/reproductor/tdd/ReproductorTest.java
@@ -121,6 +121,40 @@ public class ReproductorTest {
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Test
+	public void queNoSePuedaRegistrarUnUsuarioBasicoSiElUsernameEstaDuplicadoEnLaBaseDeDatosDeUsuarios(){
+		// Preparacion de datos
+		final String NOMBRE = "Onda Feliz";
+		Reproductor reproductor = new Reproductor(NOMBRE);
+
+		// Usuario 1
+		final String USERNAME = "jesibel12";
+		final String MAIL = "jesi@mail.com";
+		final String PASSWORD = "b@Rto1357";
+		final int DIA = 2;
+		final int MES = 11;
+		final int ANIO = 1996;
+		Usuario user1 = new UsuarioBasico(USERNAME, MAIL, PASSWORD, DIA, MES, ANIO);
+
+		// Usuario 2
+		final String USERNAME_2 = "jesibel12";
+		final String MAIL_2 = "karen@mail.com";
+		final String PASSWORD_2 = "k@reN1567";
+		final int DIA_2 = 5;
+		final int MES_2 = 10;
+		final int ANIO_2 = 1995;
+		Usuario user2 = new UsuarioBasico(USERNAME_2, MAIL_2, PASSWORD_2, DIA_2, MES_2, ANIO_2);
+
+		final Integer CANTIDAD_USUARIO_ESPERADO_UNO = 1;
+
+		// Ejecucion
+		reproductor.registrarUsuario(user1);
+
+		// Validacion
+		assertFalse(reproductor.registrarUsuario(user2));
+		assertEquals(CANTIDAD_USUARIO_ESPERADO_UNO, reproductor.obtenerCantidadUsuarios());
+	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	@Test
 	public void queSePuedaRegistrarUnUsuarioPremiumYBasicoEnLaBaseDeDatosDeUsuarios() {
 		// Preparacion de datos
 		final String NOMBRE = "Onda Feliz";
