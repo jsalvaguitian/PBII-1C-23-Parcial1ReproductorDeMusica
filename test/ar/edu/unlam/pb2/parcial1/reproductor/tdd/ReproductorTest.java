4d00c871e55dfb8546aa2c9123ef30b6b53d8d70
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2023-05-14 20:28:40 -0300

    se agrega test para registrar 2 usuarios premium

--- test/ar/edu/unlam/pb2/parcial1/reproductor/tdd/ReproductorTest.java
+++ test/ar/edu/unlam/pb2/parcial1/reproductor/tdd/ReproductorTest.java
@@ -189,6 +189,40 @@ public class ReproductorTest {
 	}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Test
+	public void queSePuedaRegistrarDosUsuariosPremiumConDatosValidos(){
+		// Preparacion de datos
+		final String NOMBRE = "Onda Feliz";
+		Reproductor reproductor = new Reproductor(NOMBRE);
+
+		// Usuario 1
+		final String USERNAME = "jesibel12";
+		final String MAIL = "jesi@mail.com";
+		final String PASSWORD = "b@Rto1357";
+		final int DIA = 2;
+		final int MES = 11;
+		final int ANIO = 1996;
+		Usuario user1 = new UsuarioPremium(USERNAME, MAIL, PASSWORD, DIA, MES, ANIO);
+
+		// Usuario 2
+		final String USERNAME_2 = "karen123";
+		final String MAIL_2 = "karen@mail.com";
+		final String PASSWORD_2 = "k@reN1567";
+		final int DIA_2 = 5;
+		final int MES_2 = 10;
+		final int ANIO_2 = 1995;
+		Usuario user2 = new UsuarioPremium(USERNAME_2, MAIL_2, PASSWORD_2, DIA_2, MES_2, ANIO_2);
+
+		final Integer CANTIDAD_USUARIOS_ESPERADOS_DOS = 2;
+
+		// Ejecucion
+		reproductor.registrarUsuario(user1);
+		reproductor.registrarUsuario(user2);
+
+		// Validacion
+		assertEquals(CANTIDAD_USUARIOS_ESPERADOS_DOS, reproductor.obtenerCantidadUsuarios());
+	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	@Test
 	public void queSePuedaRegistrarUnUsuarioPremiumYBasicoEnLaBaseDeDatosDeUsuarios() {
 		// Preparacion de datos
 		final String NOMBRE = "Onda Feliz";
